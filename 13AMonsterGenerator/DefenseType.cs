2aa0f015b56de1b862c66c1cf6f3e09d03e086ba
davidsyntex <syntex@outlook.com>
2015-01-15 17:14:58 +0100

    Add missing file

---
+++ 13AMonsterGenerator/DefenseType.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13AMonsterGenerator
+{
+    class DefenseType
+    {
+        public DefenseType(Type type)
+        {
+            switch (type)
+            {
+                case Type.Ac:
+                    SetAc();
+                    break;
+                case Type.Pd:
+                    SetMd();
+                    break;
+                case Type.Md:
+                    SetPd();
+                    break;
+                default:
+                    SetAc();
+                    break;
+            }
+        }
+
+        public DefenseType()
+        {
+        }
+
+        // ReSharper disable once UnusedAutoPropertyAccessor.Global
+        public string Name { get; private set; }
+        public string Shortname { get; private set; }
+        public int Weight { get; private set; }
+
+        private void SetAc()
+        {
+            Name = "Armour Class";
+            Shortname = "AC";
+            Weight = 12;
+        }
+
+        private void SetMd()
+        {
+            Name = "Mental Defense";
+            Shortname = "MD";
+            Weight = 4;
+        }
+
+        private void SetPd()
+        {
+            Name = "Physical Defense";
+            Shortname = "PD";
+            Weight = 4;
+        }
+
+        public enum Type
+        {
+            Ac,
+            Pd,
+            Md
+        }
+    }
+}
